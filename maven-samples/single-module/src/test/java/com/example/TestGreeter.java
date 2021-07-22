ba64c39be1f5e9b7b5d3262fbfdd09c4f8025d50
root <root@ip-172-31-1-225.ec2.internal>
2021-07-22 18:00:56 +0000

    changed file

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -38,3 +38,12 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
+
+ @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you ";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+}
+
